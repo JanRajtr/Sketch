139155b94678b8ed0d788b90ca0832a5bdf4ba73
Jan Rajtr <rajtrj@cngroup.dk>
2019-09-06 09:17:03 +0200

    - removed example - moved to source folder

--- Example/Pods/Target Support Files/Pods-Sketch_Example/Pods-Sketch_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sketch_Example : NSObject
-@end
-@implementation PodsDummy_Pods_Sketch_Example
-@end
--- Example/Pods/Target Support Files/Pods-Sketch_Tests/Pods-Sketch_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Sketch_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_Sketch_Tests
-@end
